5452a0e9a53cffc484dea03d75bbe1592f69100f
Lucas Zamprogno <lucas.zamprogno@gmail.com>
2017-05-30 15:37:26 -0700

    Initial changes, untested with extension

--- src/server.js
+++ src/server.js
@@ -2,14 +2,7 @@ var restify = require('restify');
 var fs = require('fs');
 const PORT = 4321;
 
-// For coordinate data
-var lastX = null;
-var lastY = null;
-var lastTimestamp = null;
-
-// For page change events
-var lastHref = null;
-var lastTitle = null;
+var sessions = {};
 
 function echo(req, res, next) {
 	res.send(200);
@@ -18,7 +11,11 @@ function echo(req, res, next) {
 }
 
 function sendCoords(req, res, next) {
-	if(lastX == null || lastY == null || lastTimestamp == null) {
+	var id = req.params['id'];
+	if(!sessions[id]) {
+		newSessionObject(id);
+	}
+	if(sessions[id]['lastX'] == null || sessions[id]['lastY'] == null || sessions[id]['lastTimestamp'] == null) {
 		res.send(404);
 	} else {
 		res.json(200, {'x': lastX, 'y': lastY, 'timestamp': lastTimestamp});
@@ -27,31 +24,43 @@ function sendCoords(req, res, next) {
 }
 
 function receiveCoords(req, res, next) {
-  	lastX = req.params['x'];
-  	lastY = req.params['y'];
-  	lastTimestamp = req.params['timestamp'];
+	var id = req.params['id'];
+	if(!sessions[id]) {
+		newSessionObject(id);
+	}
+  	sessions[id]['lastX'] = req.params['x'];
+  	sessions[id]['lastY'] = req.params['y'];
+  	sessions[id]['lastTimestamp'] = req.params['timestamp'];
   	res.send(200);
   	next();
 }
 
 function receiveCoordsM(req, res, next) {
-  	lastX = req.body['x'];
-  	lastY = req.body['y'];
-  	lastTimestamp = req.body['timestamp'];
+	var id = req.body['id'];
+	if(!sessions[id]) {
+		newSessionObject(id);
+	}
+  	sessions[id]['lastX'] = req.body['x'];
+  	sessions[id]['lastY'] = req.body['y'];
+  	sessions[id]['lastTimestamp'] = req.body['timestamp'];
   	res.send(200);
   	next();
 }
 
 function receiveData(req, res, next) {
-	// Do something with it
-	if(req.body.hasOwnProperty('pageHref') && req.body['pageHref'] !== lastHref) {
-		if(lastHref !== null) {
-			console.log(pageChangeObject(lastTitle, req.body['pageTitle'], lastHref, req.body['pageHref'], req.body['timestamp']));
-		}
-		lastHref = req.body['pageHref'];
-		lastTitle = req.body['pageTitle'];
+	var id = req.params['id'];
+	if(!sessions[id]) { // Really this should never hapen
+		newSessionObject(id);
 	}
-	console.log(req.body);
+	// Do something with it
+	if(req.body.hasOwnProperty('pageHref') && req.body['pageHref'] !== sessions[id]['lastHref']) {
+		if(sessions[id]['lastHref'] !== null) {
+			sessions[id]['outputStream'].write(pageChangeObject(sessions[id]['lastTitle'], req.body['pageTitle'], sessions[id]['lastHref'], req.body['pageHref'], req.body['timestamp']));
+		}
+		sessions[id]['lastHref'] = req.body['pageHref'];
+		sessions[id]['lastTitle'] = req.body['pageTitle'];
+	}
+	sessions[id]['outputStream'].write(req.body);
 	res.send(200);
 	next();
 }
@@ -65,6 +74,25 @@ function pageChangeObject(oldTitle, newTitle, oldHref, newHref, timestamp) {
 		'newHref': newHref,
 		'timestamp': timestamp
 	};
+}
+
+function newSessionObject(id) {
+	return {
+		'lastX': null,
+		'lastY': null,
+		'lastTimestamp': null,
+		'lastHref': null,
+		'lastTitle': null,
+		'outputStream': setupOutput(id)
+	};
+}
+
+function setupOutput(id) {
+	var path = './data/' + id + '.txt';
+	if(!fs.existsSync(path)) {
+		fs.closeSync(fs.openSync(path, 'w'));
+	}
+	return fs.createWriteStream(path, {flags: 'a'});
 }
 
 var server = restify.createServer({
