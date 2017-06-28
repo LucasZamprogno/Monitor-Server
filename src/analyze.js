8c568e80cfd4ab9fe37fd2f513d90e5066b9ced0
Lucas <lucas.zamprogno@gmail.com>
2017-06-28 10:31:34 -0700

    Code formatting

--- src/analyze.js
+++ src/analyze.js
@@ -350,14 +350,14 @@ function makeReadableTimeline(data) {
 			case 'gazeEnd':
 				str += 'User stopped looking at "' + obj['target'];
 				str += '", on "' + obj['pageTitle'] + '"';
-				str += ' after ' + Math.round(obj['duration']/10)/100 + ' seconds';
+				str += ' after ' + Math.round(obj['duration'] / 10) / 100 + ' seconds';
 				break;
 			case 'pageViewStart':
 				str += 'User started looking at "' + obj['domain'] + '"';
 				break;
 			case 'pageViewEnd':
 				str += 'User stopped looking at "' + obj['domain'] + '"';
-				str += ' after ' + Math.round(obj['duration']/10)/100 + ' seconds';
+				str += ' after ' + Math.round(obj['duration'] / 10) / 100 + ' seconds';
 				break;
 			case 'Page Change': // Change this after playing with old dataset
 			case 'pageChange':
