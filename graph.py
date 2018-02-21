0622669a4700b654707297db95bd163fd3651ed8
Lucas Zamprogno <lucas.zamprogno@gmail.com>
2018-02-21 15:17:55 -0800

    Clean code is happy code

--- graph.py
+++ graph.py
@@ -23,19 +23,24 @@ def read_file():
             lines.append(json.loads(line))
     return lines
 
-def combine_gazes(list):
+
+def combine_gazes(gazes):
     i = 0
     j = 1
     final = []
-    while j < len(list):
-        if list[i].should_combine(list[j]):
-            list[i].consume(list[j])
+    while j < len(gazes):
+        if gazes[i].should_combine(gazes[j]):
+            gazes[i].consume(gazes[j])
             j += 1
         else:
-            final.append(list[i])
+            final.append(gazes[i])
             i = j
             j += 1
     return final
+
+
+def is_diff_gaze(line):
+    return (line['type'] == 'gaze') and ('index' in line) and (line['target'] != 'fileCode')
 
 
 class Commit:
@@ -54,31 +59,29 @@ class Commit:
     def graph(self, lines):
         x_values = []
         y_values = []
-        bar_values = []
         bar_colors = []
         min_timestamp = lines[0].timestamp
         max_timestamp = lines[-1].timestamp_end - min_timestamp
         t = min_timestamp
-        i = 0
+        i = 0  # Gaze selector
         while i < len(lines):
-            if lines[i].timestamp <= t and t < lines[i].timestamp_end:
+            if lines[i].timestamp <= t < lines[i].timestamp_end:  # In current gaze
                 x_values.append(t - min_timestamp)
                 y_values.append(lines[i].commit_relative_index)
                 t += args.sample
             elif t < lines[i].timestamp:  # In a gap
                 t += args.sample
-            else:
+            else:  # Past current gaze
                 i += 1
         for diff in self.diffs:
             for line in diff.lines:
-                bar_values.append(max_timestamp)
                 bar_colors.append(Commit.colorize(line.type))
-            bar_values.append(max_timestamp)
             bar_colors.append('#000000')
         bar_colors = bar_colors[:-1]  # flip so 0 is at top
         fig, ax = plt.subplots()
-        ax.barh(range(len(bar_values)), bar_values, height=1, color=bar_colors)
+        ax.barh(range(len(bar_colors)), [max_timestamp for _ in bar_colors], height=1, color=bar_colors)
         ax.scatter(x_values, y_values, zorder=10, s=2)
+        # Manually do the legend
         green_throwaway = plt.Line2D((0, 1), (0, 0), color='#bef5cb')
         red_throwaway = plt.Line2D((0, 1), (0, 0), color='#fdaeb7')
         white_throwaway = plt.Line2D((0, 1), (0, 0), color='#ffffff')
@@ -87,12 +90,13 @@ class Commit:
         plt.legend([green_throwaway, red_throwaway, white_throwaway, blue_throwaway, black_throwaway],
                    ['Addition', 'Deletion', 'Unchanged', 'Expandable', 'Diff separator'],
                    bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.)
+        # /legend
         plt.xlabel('Relative timestamp (ms)')
-        plt.ylabel('Diff line')
-        plt.ylim(len(bar_values) - 1, 0)
+        plt.ylabel('Commit line')
+        plt.ylim(len(bar_colors) - 1, 0)
         plt.xlim(0, max_timestamp)
         plt.savefig(path_out + args.filename + '/' + self.href + '.pdf', type='pdf')
-        if args.show:
+        if args.show:  # Order is important, this has to be after save or it saves blank
             plt.show()
 
     @staticmethod
@@ -111,7 +115,6 @@ class Commit:
 
 class Diff:
     def __init__(self, obj):
-        self.file = obj['file']
         self.href = obj['pageHref']
         self.index = obj['diffIndex']
         self.lines = [Line(x) for x in obj['allLineDetails']]
@@ -122,18 +125,17 @@ class Diff:
             self.make_index_map()
 
     def make_index_map(self):
-        map = {}
+        index_map = {}
         offset = 0
         for i in range(len(self.lines) - 1):
-            map[self.lines[i].id] = self.lines[i].index + offset
-            self.lines[i].diff_relative_index = self.lines[i].index + offset
+            index_map[self.lines[i].id] = self.lines[i].index + offset
+            # If there is a side by side add/del pair, increase the offset
             if (self.lines[i].type == 'deletion' and
-                    self.lines[i+1].type == 'addition' and
-                        self.lines[i].index == self.lines[i+1].index):
+                self.lines[i+1].type == 'addition' and
+                self.lines[i].index == self.lines[i+1].index):
                 offset += 1
-        map[self.lines[i+1].id] = self.lines[i+1].index + offset  # inside loop would break conditional
-        self.lines[i+1].diff_relative_index = self.lines[i+1].index + offset
-        self.map = map
+        index_map[self.lines[i+1].id] = self.lines[i+1].index + offset  # inside loop would break conditional
+        self.map = index_map
 
 
 class Gaze:
@@ -142,9 +144,7 @@ class Gaze:
         self.timestamp = obj['timestamp']
         self.timestamp_end = obj['timestampEnd']
         self.duration = obj['duration']
-        self.domain = obj['domain']
         self.href = obj['pageHref']
-        self.page_type = obj['pageType']
 
 
 class Line:
@@ -152,27 +152,16 @@ class Line:
         self.index = obj['index']
         self.diff_index = obj['diffIndex']
         self.file = obj['file']
-        self.text = obj['codeText']
-        self.diff_relative_index = None
         self.commit_relative_index = self.index
         if obj['target'] == 'Expandable line details' or \
                         obj['target'] == 'File start marker' or \
                         obj['target'] == 'File end marker' or \
                         obj['target'] == 'Expandable line button':
             self.type = 'expandable'
-            self.old_start = obj['oldStart']
-            self.old_end = obj['oldEnd']
-            self.new_start = obj['newStart']
-            self.new_end = obj['newEnd']
             self.is_change = False
         else:
             self.type = obj['change']
             self.is_change = True if self.type != 'unchanged' else False
-            self.old_line_num = obj['oldLineNum']
-            self.new_line_num = obj['newLineNum']
-            self.length = obj['length']
-            self.indent = obj['indentValue']
-            self.indent_type = obj['indentType']
         self.id = str(self.index) + '-' + self.type
 
 
@@ -193,24 +182,28 @@ class LineGaze(Line, Gaze):
         self.timestamp_end = other.timestamp_end
         self.duration = self.timestamp_end = self.timestamp
 
+
 def run():
     jsons = read_file()
-    line_gazes = [LineGaze(x) for x in jsons if ((x['type'] == 'gaze') and ('index' in x))]
+    line_gazes = [LineGaze(x) for x in jsons if is_diff_gaze(x)]
+    # For each saved set of divs, make a commit object, containing diff objects
     commits = [Commit(x['pageHref'], [Diff(y) for y in x['diffs'] if y is not None])
                for x in jsons if x['type'] == 'diffs']
     line_gazes.sort(key=lambda x: x.timestamp)
-    line_gazes_combined = combine_gazes(line_gazes)
-    line_gazes_combined = [x for x in line_gazes_combined if x.duration > args.ignore]
-    for line in line_gazes_combined:
+    line_gazes = combine_gazes(line_gazes)
+    line_gazes = [x for x in line_gazes if x.duration > args.ignore]
+    # For each line, find its source diff, get its offset
+    for line in line_gazes:
         for commit in commits:
             for diff in commit.diffs:
                 if line.href + '-' + line.diff_index == diff.id:
                     if args.split:
                         line.commit_relative_index = diff.map[line.id]
                     line.commit_relative_index += diff.offset
-                    break  # Doesn't break commit loop but oh well, minor
+                    break  # Doesn't break commit loop but oh well, minor inefficiency
+    # Extract the gaze points that go with each commit, graph those gazes on that commit
     for commit in commits:
-        commit.graph([x for x in line_gazes_combined if x.href == commit.href])
+        commit.graph([x for x in line_gazes if x.href == commit.href])
 
 
 run()
